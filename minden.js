321f32630c662d325205c9b128ce645520dbe9f8
bartahunor <barta.hunor.attila@diak.szbi-pg.hu>
2025-10-24 17:15:59 +0200

    webshop1

--- minden.js
+++ minden.js
@@ -54,3 +54,40 @@ window.addEventListener("DOMContentLoaded", async () => {
 });
 
 
+
+window.addEventListener("scroll", function(e) {
+  last_known_scroll_position = window.scrollY;
+
+  if (!ticking) {
+    window.requestAnimationFrame(function() {
+      scrollEvent(last_known_scroll_position);
+      ticking = false;
+    });
+  }
+
+  ticking = true;
+});
+
+
+
+function isInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+      rect.top < window.innerHeight &&
+      rect.bottom > 0
+    );
+  }
+
+  function checkScrollAnimations() {
+    const elements = document.querySelectorAll('.scroll-anim');
+    elements.forEach(el => {
+      if (isInViewport(el)) {
+        el.classList.add('in-view');
+      } else {
+        el.classList.remove('in-view');
+      }
+    });
+  }
+
+  document.addEventListener('scroll', checkScrollAnimations);
+  document.addEventListener('DOMContentLoaded', checkScrollAnimations);
